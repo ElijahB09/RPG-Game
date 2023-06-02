02177a4aa8fb308ff2435a70d433fa644e9d0a74
Elijah-Brady <117706158+Elijah-Brady@users.noreply.github.com>
2023-06-01 21:41:47 -0500

    Small optimization changes to PlayerController

--- Backend/game/src/main/java/com/rpg/game/controllers/PlayerController.java
+++ Backend/game/src/main/java/com/rpg/game/controllers/PlayerController.java
@@ -4,10 +4,8 @@ import com.rpg.game.entities.PlayerEntity;
 import com.rpg.game.repositories.PlayerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 @RestController
