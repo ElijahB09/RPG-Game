6bebdcb89e8c15f4192be6c13c20e65401f0ffb7
Elijah-Brady <117706158+Elijah-Brady@users.noreply.github.com>
2023-06-02 19:35:42 -0500

    Attempt to gitignore env.properties

---
+++ Backend/game/src/test/java/com/rpg/game/smoke_tests/SmokeTest.java
@@ -0,0 +1,17 @@
+package com.rpg.game.controller_tests;
+
+import com.rpg.game.controllers.PlayerController;
+import com.rpg.game.repositories.PlayerRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PlayerControllerTest {
+
+    @Autowired
+    private PlayerController playerController;
+
+    @Test
+    void 
+}
