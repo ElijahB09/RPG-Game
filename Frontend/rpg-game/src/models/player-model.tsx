d0b237a3a261aaaf350cf00db02b2ef915bacbe6
Elijah-Brady <117706158+Elijah-Brady@users.noreply.github.com>
2023-06-03 14:58:47 -0500

    Created axios requests, working through hooking it up to a webpage

--- Frontend/rpg-game/src/models/player-model.tsx
+++
@@ -1,7 +0,0 @@
-export default interface player {
-    id: number,
-    health: number,
-    strength: number,
-    defence: number,
-    name: string
-}
--- Frontend/rpg-game/src/views/testing-views.tsx
+++ Frontend/rpg-game/src/views/testing-views.tsx
@@ -1,4 +1,15 @@
+import { PostPlayer } from "../api/Player-Api";
+import Player from "../models/Player-Model";
+
 export default function Viewtest () {
+    const player: Player = {
+        "id": 0,
+        "strength": 13,
+        "defence": 14,
+        "health": 90,
+        "name": "Test-User"
+    }
+    PostPlayer(player)
     return (
     <div>
         <header>
