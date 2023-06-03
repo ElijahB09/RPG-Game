b28e47ae90c4beccfedc1a3bc6a6355929c9b86e
Elijah-Brady <117706158+Elijah-Brady@users.noreply.github.com>
2023-06-02 19:46:54 -0500

    Finished setting up the player obj with getters and setters

--- Backend/game/src/main/java/com/rpg/game/entities/PlayerEntity.java
+++ Backend/game/src/main/java/com/rpg/game/entities/PlayerEntity.java
@@ -22,4 +22,44 @@ public class PlayerEntity {
 
     @Column(name = "name")
     private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
